900b313aaa46bba5df729afcc3eabdca30fd5a61
kumaracb <kumaracb@gmail.com>
2024-02-25 12:47:05 +0530

    list and set topics

---
+++ 04_07Feb2024/Lab022.py
@@ -0,0 +1,31 @@
+# Boolean
+# Data Type - True and False
+isKumaraMarried = True
+# isAmitMarried - False
+
+is_kumara_married  =True
+
+
+var_name_input_21 = "Kumara"
+
+varKumara = 38
+var_kumara = 38 #Good practice - C
+
+# LIst - Unordered collection of Items
+my_list = [1, 2, 3, 4, 5, 4]
+print(my_list)
+print(type(my_list))
+
+# Sets - Unordered collection of Unique Items
+my_set = {1, 2, 3, 4, 5, 4}
+print(my_set)
+print(type(my_set))
+
+
+
+
+
+
+
+
+
