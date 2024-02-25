4b17a0ffbc2c92c4f68d0a88276d043b7a5ebb0c
kumaracb <kumaracb@gmail.com>
2024-02-25 13:46:12 +0530

    Decimal, Octa and hexa

---
+++ 04_07Feb2024/Lab025.py
@@ -0,0 +1,21 @@
+# Decimal
+age = 38
+# Binary -> 0, 1 -> binary
+number_ten = 0b1010
+# Oct
+c = 0o130
+# Hex
+d = 0x12c
+
+# String
+name = "Kumara"
+name2 = 'Kumara'
+mul_string_line = """This is multiline string we can have line which you can type anything like story"""
+
+# Boolean Literals
+x = True
+y = False
+
+# == -> Compare operator == True
+v1 = (1 == True)
+print(v1)
