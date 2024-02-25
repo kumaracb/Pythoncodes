fa53646aa9ba07500eb1252bb18cd7b8abbd3948
kumaracb <kumaracb@gmail.com>
2024-02-25 23:15:51 +0530

    Tilt opertor

---
+++ Week#2Task1/Task#1.3-Tilt-operator.py
@@ -0,0 +1,8 @@
+# What does the ^ operator do in Python, and in what context is it commonly used?
+# The ^ operator will perform the operation of XOR, like whenever both the inputs are true/false
+# then result will be "false" and either any one of input is true then result will be "true"
+
+a = int(input("Enter the value 1 "))
+b = int(input("Enter the Value 2 "))
+c = str(a ^ b )
+print(c)
