5041fcf79024d405f90c3cfefafa5002590f40b5
kumaracb <kumaracb@gmail.com>
2024-02-25 14:38:03 +0530

    Logical Operator

---
+++ 04_07Feb2024/Lab028.py
@@ -0,0 +1,22 @@
+# Logical Operator
+x = 10
+y = 20
+print(x > y)
+print(x < y)
+
+a = 10
+b = 10
+print(a >= b)
+print(a > b)
+print(a < b)
+
+# a b c (Truth table of AND)
+# 0 0 0
+# 0 1 0
+# 1 0 0
+# 1 1 1
+
+f = False
+t = True
+print(f and t)  # Truth Table of and
+print(f or t) # Truth Table of or
