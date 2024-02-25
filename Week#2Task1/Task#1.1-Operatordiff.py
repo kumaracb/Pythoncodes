798cc609566cfc86c694187bceeb37ccc5c0ff59
kumaracb <kumaracb@gmail.com>
2024-02-25 22:42:08 +0530

    Difference of = and ==

---
+++ Week#2Task1/Task#1.1-Operatordiff.py
@@ -0,0 +1,5 @@
+# 1 Explain the difference between the = operator and the == operator in Python.
+
+# = -> This is simple assignment operator and it assign the values from right operand to left operand
+# == -> This will check the value of two operands are equal or not.
+
