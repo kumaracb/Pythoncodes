ab5aa65df59ad3f6db47a16ae917c91a353b3792
kumaracb <kumaracb@gmail.com>
2024-02-26 03:02:10 +0530

    loops

---
+++ 05_09Feb2024/Lab039.py
@@ -0,0 +1,4 @@
+# Print the value of Hello Kumara 10 times
+
+for i in range(1,11): # 1-10
+    print("Hello Kumara", i)
---
+++ 05_09Feb2024/lab038.py
@@ -0,0 +1,28 @@
+# Loop ?
+# # Repeat a block of code multiple times
+
+for i in range(1, 10): # 1-9
+    print("Hello PythonWorld")
+    print(i)
+
+print("----------------")
+
+for i in range(5): # 0-4
+    print(i)
+
+print("----------------")
+
+for i in range(1,5): # 1-4
+    print(i)
+
+print("----------------")
+
+for i in range(3,5): # 3-4
+    print(i)
+
+print("----------------")
+
+for i in range(1,10,2): # 1-9,3,5,7,9
+    print(i)
+
+# range(start, stop step)
