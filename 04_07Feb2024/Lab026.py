7f7fcac8007ff091f0ae8129da3d42fb2286b79f
kumaracb <kumaracb@gmail.com>
2024-02-25 14:04:46 +0530

    Assignment Operator

---
+++ 04_07Feb2024/Lab026.py
@@ -0,0 +1,24 @@
+# Assignment Operator
+name = "Kumara"
+# It will stor the value of variable literal to the identifier
+
+# Unary Operator - Single value or literal
+age = 100
+my_bank_bal = -100
+# print(age)
+# print(my_bank_bal)
+
+# Not Operator - Unary (booleans)
+is_married = True
+# print(not is_married)
+
+# is operstor - Identity Operator
+a = 5
+b = 6
+c = False
+print(a is b) #True
+# print(a is c) #False
+#
+# my_list = [1, 2, 3]
+# my_list2 = [1, 2, 3]
+# print(my_list is my_list2)
