88e369b8bd301e96ef85ba890305a93b85a9588d
kumaracb <kumaracb@gmail.com>
2024-02-25 23:57:48 +0530

    Week#2 assignments

---
+++ Week#2Task2/Task#2.2-Number.py
@@ -0,0 +1,11 @@
+# Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+a = int(input("Enter the first number "))
+b = int(input("Enter the second number "))
+print("both the numbers are equal", a == b)
+print("a is grater than b", a > b)
+print("b is grater than a", b >a)
+
+print(a == b)
+print(a > b)
+print(a < b)
---
+++ Week#2Task2/Task#2.3-SquareCube.py
@@ -0,0 +1,7 @@
+# Develop a Python script that calculates the square and cube of a given number.
+
+a = int(input("Enter teh Sqaure Number "))
+b = int(input("Enter the Cube Number "))
+print("The Square of a is: ", a**2)
+print("The Cube of b is: ", b**3)
+
