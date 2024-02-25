0136f0b6e22d5cfe354288345d161a8f8056a332
kumaracb <kumaracb@gmail.com>
2024-02-25 13:07:32 +0530

    Literals

---
+++ 04_07Feb2024/Lab023.py
@@ -0,0 +1,22 @@
+# Literals
+
+# var_name = var_value
+# var_name -> Identifir
+# var_name -> Literals
+
+# Literals are the actual values assigned
+# Literals can be Numeric and Non-Numeric
+name = "Kumara"
+age = 38
+pi = 3.142
+char = 'C'
+is_married = True
+have_inova = None
+my_list = []
+my_set = {}
+print(type(char))
+print(type(age))
+print(type(is_married))
+print(type(have_inova))
+print(type(my_set))
+print(type(my_list))
